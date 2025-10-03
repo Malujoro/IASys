aac7ffce9c8e8d7082352369b4f6918390fe37f9
Malujoro <mateus_rocha_sousa@hotmail.com>
2025-10-02 21:34:53 -0300

    Ajuste no nome da pasta Ajuste na espera de conexão do rabbit

--- messageGenerator/src/main/java/com/example/MessageGenerator.java
+++ messageGenerator/src/main/java/com/example/MessageGenerator.java
@@ -26,28 +26,33 @@ public class MessageGenerator {
         factory.setUsername("guest");
         factory.setPassword("guest");
 
-        try (Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel()) {
-            String exchangeName = "images";
-            channel.exchangeDeclare(exchangeName, "topic");
+        while(true) {
+            try (Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()) {
+                String exchangeName = "images";
+                channel.exchangeDeclare(exchangeName, "topic");
 
-            int messagesPerSecond = 5;
-            long delay = 1000 / messagesPerSecond;
+                int messagesPerSecond = 5;
+                long delay = 1000 / messagesPerSecond;
 
-            while (true) {
-                String message = generateMessage();
+                while (true) {
+                    String message = generateMessage();
 
-                String routingKey;
-                if (message.contains("feliz") || message.contains("triste")) {
-                    routingKey = "face";  // é rosto de pessoa
-                } else {
-                    routingKey = "team";  // é brasão de time
+                    String routingKey;
+                    if (message.contains("feliz") || message.contains("triste")) {
+                        routingKey = "face";  // é rosto de pessoa
+                    } else {
+                        routingKey = "team";  // é brasão de time
+                    }
+
+                    channel.basicPublish(exchangeName, routingKey, null, message.getBytes("UTF-8"));
+                    System.out.println("[ENVIADO] " + message);
+
+                    Thread.sleep(delay);
                 }
-
-                channel.basicPublish(exchangeName, routingKey, null, message.getBytes("UTF-8"));
-                System.out.println("[ENVIADO] " + message);
-
-                Thread.sleep(delay);
+            } catch (Exception e) {
+                System.out.println("RabbitMQ não disponível ainda...");
+                Thread.sleep(3000);
             }
         }
     }
